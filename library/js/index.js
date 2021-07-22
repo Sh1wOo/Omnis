7e35d56b6d9404d4f673bc12a5a4f30266678eae
NoPoker <pisun333256q28@gmail.com>
2021-07-23 02:54:38 +0300

    new futures

--- library/js/index.js
+++ library/js/index.js
@@ -3,4 +3,47 @@ const print = (something) => {
 }
 
 const preloaderLib = document.querySelector('preloaderlib');
-print(preloaderLib);
+
+const sNameWhite = document.querySelector('preloaderlib[s-name="white"]');
+const sNameDark = document.querySelector('preloaderlib[s-name="dark"]');
+const sNameYellow = document.querySelector('preloaderlib[s-name="yellow"]');
+
+const sNameDetect = document.querySelector(`preloaderlib`);
+
+const detectedSname = sNameDetect.attributes[0].value;
+const sNameOur = document.querySelector(`preloaderlib[s-name="${detectedSname}"]`);
+// print(detectedSname);
+
+
+function changeOurColor() {
+    document.body.style.backgroundColor = `${detectedSname}`;
+    print(`${detectedSname} is detected`);
+} 
+function changeColorDark() {
+    document.body.style.backgroundColor = "#2C2F33";
+    print('dark is detected')
+}
+function changeColorWhite() {
+    document.body.style.backgroundColor = "#FFFFFF";
+    print('white is detected')
+}
+function changeColorYellow() {
+    document.body.style.backgroundColor = "#e8e85f";
+    print('yellow is detected')
+}
+
+switch (preloaderLib) {
+    case sNameWhite:
+        changeColorWhite();
+        break;
+    case sNameYellow:
+        changeColorYellow();
+        break;
+    case sNameOur:
+        changeOurColor()
+        break;
+    default:
+        changeColorDark();
+        break;
+}
+
